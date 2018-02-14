708f5c7092b32f800cedaee178ef433d7b9db60d
neillmurdoch <neill.murdoch@gmail.com>
2018-02-14 12:41:06 +0000

    Add project files.

---
+++ OdeToFood/IGreeter.cs
@@ -0,0 +1,15 @@
+﻿namespace OdeToFood
+{
+    public interface IGreeter
+    {
+        string GetMessageOfTheDay();
+    }
+
+    class Greeter : IGreeter
+    {
+        public string GetMessageOfTheDay()
+        {
+            return "Greetings!";
+        }
+    }
+}
