aecfe7df2e6d54b21e164237150ca92f0d406cb7
neillmurdoch <neill.murdoch@gmail.com>
2018-02-18 20:14:37 +0000

    Testing to a new branch

--- OdeToFood/Migrations/OdeToFoodDBContextModelSnapshot.cs
+++ OdeToFood/Migrations/OdeToFoodDBContextModelSnapshot.cs
@@ -2,12 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
-using Microsoft.EntityFrameworkCore.Storage;
-using Microsoft.EntityFrameworkCore.Storage.Internal;
 using OdeToFood.Data;
-using OdeToFood.Models;
-using System;
 
 namespace OdeToFood.Migrations
 {
